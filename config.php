6fa728ec6a3e04ffe250961a193020fcd1608362
Nyanta-23 <muhamadilhan02404@gmail.com>
2023-06-26 09:09:17 +0700

    adding index html with BROWSTEP

--- config.php
+++ config.php
@@ -1,16 +1,18 @@
 <?php
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
-error_reporting(E_ALL);
-$base_url_admin = "http://localhost/web-pariwisata/admin";
-$databaseHost = 'localhost';
-$databaseName = 'pariwisata';
-$databaseUsername = 'root';
-$databasePassword = '';
+// ini_set('display_errors', '1');
+// ini_set('display_startup_errors', '1');
+// error_reporting(E_ALL);
+// $base_url_admin = "http://localhost/web-pariwisata/admin";
+// $databaseHost = 'localhost';
+// $databaseName = 'pariwisata';
+// $databaseUsername = 'root';
+// $databasePassword = '';
 
-$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName);
-if (!$mysqli) {
-    die("<script>alert('Gagal tersambung dengan database.')</script>");
-} else {
-    // die("<script>alert(' tersambung dengan database.')</script>");
-}
+// $mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName);
+// if (!$mysqli) {
+//     die("<script>alert('Gagal tersambung dengan database.')</script>");
+// } else {
+//     // die("<script>alert(' tersambung dengan database.')</script>");
+// }
+
+// Mattin dulu, klo ga mati ga jalan. Asik
